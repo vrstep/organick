1a37b22c0b13aea064ea3f39ba8ae4b1ec9b0e69
Amir <amir.batyrggaliev@gmail.com>
2023-07-22 17:48:08 +0500

    Connect project to db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../lib/mongodb";
 
 export default NextAuth({
     providers: [
@@ -9,4 +11,5 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
+    adapter: MongoDBAdapter(clientPromise),
 });
