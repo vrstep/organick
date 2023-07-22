deb3772eaf2ad827e0845d8c43b3ad0a383812c9
Amir <amir.batyrggaliev@gmail.com>
2023-07-22 15:05:05 +0500

    Setup google auth

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-    return <>hello</>;
+    const { data: session } = useSession();
+    if (!session) {
+        // if not logged in this should appear
+        return (
+            <div>
+                <button onClick={() => signIn("google")}>
+                    Fuck you Google
+                </button>
+            </div>
+        );
+    }
+
+    return <div>logged in {session.user.name}</div>;
 }
